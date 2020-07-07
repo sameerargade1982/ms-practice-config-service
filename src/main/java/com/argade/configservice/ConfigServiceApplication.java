0b080bacc06e19bb00339b7b1f2c7f2ab195d428
Sameer Argade <sameer.argade@dws.com.au>
2020-07-07 15:12:18 +1000

    Made Config Server a eureka service client

--- src/main/java/com/argade/configservice/ConfigServiceApplication.java
+++ src/main/java/com/argade/configservice/ConfigServiceApplication.java
@@ -2,6 +2,8 @@ package com.argade.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
